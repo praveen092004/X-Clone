93ae9554788153605713e9779a72dbd0fbf04697
praveen092004 <praveenselvaraj2004@gmail.com>
2025-07-14 22:39:24 +0530

    Second Commit

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ app.use('/api/notifications', notificationRoute);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "frontend", "build")));
-    app.get("/{*all}", (req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
     });
 }
